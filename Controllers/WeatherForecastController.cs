cc3ed02f55c15386eaab7bfcccd1175973544c65
Cristiana Grigoriu <Cristiana.Grigoriu@centric.eu>
2021-08-01 16:12:55 +0300

    Add rough implementation for cosmos db with get all call

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Shopping_List.Controllers
 {
@@ -17,15 +19,22 @@ namespace Shopping_List.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ICosmosDbService _cosmosDbService;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            ICosmosDbService cosmosDbService)
         {
             _logger = logger;
+            _cosmosDbService = cosmosDbService;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
+            //var item = await this._cosmosDbService.GetItemAsync("work");
+            var items = await _cosmosDbService.GetMultipleAsync("SELECT * FROM c");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -35,5 +44,69 @@ namespace Shopping_List.Controllers
             })
             .ToArray();
         }
+    }
+
+    public class CosmosDbService : ICosmosDbService
+    {
+        private Container _container;
+
+        public CosmosDbService(
+            CosmosClient dbClient,
+            string databaseName,
+            string containerName)
+        {
+            this._container = dbClient.GetContainer(databaseName, containerName);
+        }
+
+        public async Task<Item> GetItemAsync(string id)
+        {
+            try
+            {
+                ItemResponse<Item> response = await this._container.ReadItemAsync<Item>("work", new PartitionKey("partitionKey"));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+
+        }
+
+        public async Task<IEnumerable<Item>> GetMultipleAsync(string queryString)
+        {
+            var query = _container.GetItemQueryIterator<Item>(new QueryDefinition(queryString));
+            var results = new List<Item>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
+            return results;
+        }
+    }
+
+    public interface ICosmosDbService
+    {
+        //Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<IEnumerable<Item>> GetMultipleAsync(string queryString);
+        Task<Item> GetItemAsync(string id);
+        //Task AddItemAsync(Item item);
+        //Task UpdateItemAsync(string id, Item item);
+        //Task DeleteItemAsync(string id);
+    }
+
+    public class Item
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "isComplete")]
+        public bool Completed { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shopping_List.Controllers;
 
 namespace Shopping_List
 {
@@ -28,6 +30,8 @@ namespace Shopping_List
             {
                 configuration.RootPath = "ClientApp/build";
             });
+            services.AddSingleton<ICosmosDbService>(
+                InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,6 +70,20 @@ namespace Shopping_List
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
+        }
+
+        private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+            string databaseName = configurationSection.GetSection("DatabaseName").Value;
+            string containerName = configurationSection.GetSection("ContainerName").Value;
+            string account = configurationSection.GetSection("Account").Value;
+            string key = configurationSection.GetSection("Key").Value;
+            Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
+            CosmosDbService cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+            Microsoft.Azure.Cosmos.DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/category");
+
+            return cosmosDbService;
         }
     }
 }
