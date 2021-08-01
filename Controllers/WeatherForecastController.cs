d7ad98b7582e965b8c672cc5f8fc40b74d86c01a
Cristiana Grigoriu <Cristiana.Grigoriu@centric.eu>
2021-08-01 10:46:20 +0300

    Change descriptions

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Shopping_List.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Very cool", "Very freezing", "Very hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
