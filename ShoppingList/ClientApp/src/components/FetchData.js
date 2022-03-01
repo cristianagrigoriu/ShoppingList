ee91a0b69568906fd18db4469e19d4a412ec76e1
Grigoriu <Cristiana.Grigoriu@centric.eu>
2022-03-01 21:35:57 +0200

    Use cards from react bootstrap instead of a table

--- ShoppingList/ClientApp/src/components/FetchData.js
+++ ShoppingList/ClientApp/src/components/FetchData.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
-import Toast from 'react-bootstrap/Toast';
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export class FetchData extends Component {
-  static displayName = 'New name';
+  static displayName = "New name";
 
   constructor(props) {
     super(props);
@@ -14,90 +17,73 @@ export class FetchData extends Component {
   }
 
   static renderShoppingListsTable(shoppingLists) {
-    return (
-      
-      <table className="table table-striped" aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Category</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>IsCompleted</th>
-            <th>Creation Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {shoppingLists.map((shoppingList) => (
-            <tr key={shoppingList.category}>
-              <td>{shoppingList.category}</td>
-              <td>{shoppingList.name}</td>
-              <td>{shoppingList.description}</td>
-              <td>{shoppingList.isCompleted ? "True" : "False"}</td>
-              <td>{shoppingList.dateTimeAdded}</td>
-              <td>
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={this.deleteList}
-                >
-                  Delete
-                </button>
-                  </td>
-                  <td>
-                  <Toast>
-                  <Toast.Header>
-                  <strong className="mr-auto">React-Bootstrap</strong>
-                  </Toast.Header>
-                  <Toast.Body>Tralala</Toast.Body>
-                    </Toast>
-                  </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+    return shoppingLists.map((shoppingList) => (
+      <Row xs={1} md={4} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Header>{shoppingList.category}</Card.Header>
+              <Card.Body>
+                <Card.Title>{shoppingList.name}</Card.Title>
+                <Card.Text>{shoppingList.description}</Card.Text>
+                <Button variant="primary">See more details</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    ));
   }
 
-  addList()
-  {
-      console.log("DA");
-      const response = fetch('shoppingLists', {
-          method: 'POST',
-          body: JSON.stringify({
-              category: 'Home',
-              name: 'Cleaning Supplies',
-              description: 'For the spring cleaning'
-          }),
-          headers: {
-              "Content-type": "application/json; charset=UTF-8"
-          }
-      }).then(response => {
-          return response.json()
-      });
+  addList() {
+    console.log("DA");
+    const response = fetch("shoppingLists", {
+      method: "POST",
+      body: JSON.stringify({
+        category: "Home",
+        name: "Cleaning Supplies",
+        description: "For the spring cleaning",
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => {
+      return response.json();
+    });
   }
 
-  deleteList()
-  {
-      console.log("Delete");
+  deleteList() {
+    console.log("Delete");
   }
 
   render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderShoppingListsTable(this.state.shoppingLists);
+    let contents = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      FetchData.renderShoppingListsTable(this.state.shoppingLists)
+    );
 
     return (
       <div>
-        <h1 id="tabelLabel" >Shopping Lists</h1>
+        <h1 id="tabelLabel">Shopping Lists</h1>
         <p>This component demonstrates fetching data from the server.</p>
-            <button type="button" className="btn btn-primary" onClick={this.addList}>Add List</button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.addList}
+        >
+          Add List
+        </button>
         {contents}
       </div>
     );
   }
 
   async populateWeatherData() {
-    const response = await fetch('shoppinglists');
+    const response = await fetch("shoppinglists");
     const data = await response.json();
     this.setState({ shoppingLists: data, loading: false });
   }
